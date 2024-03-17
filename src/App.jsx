ac9fec10ac50290d683378c692bb4dbbe403cc08
wharton-git <whartonaldrick@gmail.com>
2024-03-17 19:12:25 +0300

    "edit: Presentation"

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 /* eslint-disable react/no-unescaped-entities */
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
@@ -13,22 +11,10 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        It's My new Portfolio Site, based on REACT and VITE (You will see al the content soon ... ) <br/>
+        Thanks for Watching
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          WHART : It's My New Portfolio
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
